513c9318e4682da0a7e94f4222ad64178a3d1eae
ryohey <r.k.blvdr+github@gmail.com>
2016-11-09 17:27:10 +0900

    auth.js に initFirebaseIfNeeded と isLoggedIn を追加

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import firebase from "firebase"
+import { initFirebaseIfNeeded } from "./helpers/auth"
 
 class App extends Component {
-
   constructor(props) {
     super(props)
-
-    const config = {
-      apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-      authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-      databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-      storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
-    }
-
-    if (!config.apiKey) {
-      throw new Error("API KEY not found. Please read README.md")
-    }
-
-    firebase.initializeApp(config)
+    initFirebaseIfNeeded();
   }
 
   render() {
--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,5 +1,30 @@
 import firebase from "firebase"
 
+export const initFirebaseIfNeeded = () => {
+  if (firebase.apps.length > 0) {
+    // 初期化済みなら何もしない
+    return
+  }
+
+  const config = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+  }
+
+  if (!config.apiKey) {
+    throw new Error("API KEY not found. Please read README.md")
+  }
+
+  firebase.initializeApp(config)
+}
+
+export const isLoggedIn = () => {
+  return firebase.apps.length > 0 && firebase.auth().currentUser
+}
+
 export const signIn = () => {
   const provider = new firebase.auth.GithubAuthProvider()
   return firebase.auth().signInWithPopup(provider)
@@ -8,4 +33,3 @@ export const signIn = () => {
 export const signOut = () => {
   return firebase.auth().signOut()
 }
-
