b42d7ecebb10a68d43f5f0f70850f4d88d9ca38d
ryohey <r.k.blvdr+github@gmail.com>
2016-11-10 12:24:37 +0900

    イベントの表示をいい感じにする

--- src/components/EventTable.js
+++ src/components/EventTable.js
@@ -5,57 +5,53 @@ import moment from "moment"
 import "moment-range"
 moment.locale("ja")
 
-export default function EventTable(props) {
+import "./EventTable.css"
 
+function EventItem(props) {
+  const e = props.event
+  return <div className="EventItem">
+    <p className="content">{e.event}</p>
+    <p className="name">{e.name}</p>
+  </div>
+}
+
+export default function EventTable(props) {
   const { events, start, end } = props
+
   if (!events) {
     return null
   }
 
   const days = moment.range(start, end).toArray("days")
 
-  const groupedEvents = _(events).entries()
-    .map(e => Object.assign(e[1], {key: e[0]}, {dayStr: moment(e[1].created_at).format("MM/DD (dd)")}))
-    .groupBy("dayStr")
-    .value()
+  const head = days.map(d =>
+    <th key={d.valueOf()}>
+      <p className="date">{d.format("MM/DD")}</p>
+      <p className="dow">{d.format("dd")}</p>
+    </th>
+  )
 
-  const transposedEvents = (() => {
-    const h = _.map(days, d => {
-      const dayStr = d.format("MM/DD (dd)")
-      return groupedEvents[dayStr] || []
-    })
+  const eventsWithKey = _.entries(events).map(e => ({key: e[0], ...e[1]}))
 
-    return _.map(h, (a, i) => {
-      return _.map(h, b => {
-        return b[i]
-      })
-    })
-  })()
+  const columns = days.map(d => {
+    // この日のイベント
+    const eventsForDay = _.values(eventsWithKey).filter(e => moment(e.created_at).isSame(d, "day"))
 
-  const head = _.map(days, d => <th key={`head_${d.valueOf()}`}>{d.format("MM/DD (dd)")}</th>)
-
-  const body = _.map(transposedEvents, (a, i) => {
-
-    const tds = _.map(a, (b, j) => {
-      if (b) {
-        return <td key={b.key}>{b.event}</td>
-      } else {
-        return <td key={`${i}-${j}`}></td>
-      }
-    })
-    return <tr key={i}>
-      {tds}
-    </tr>
+    return <td key={d.valueOf()}>
+      {eventsForDay.map(e => <EventItem event={e} key={e.key} />)}
+    </td>
   })
 
-  return <table>
+  return <table className="EventTable">
     <thead>
       <tr>
         {head}
       </tr>
     </thead>
     <tbody>
-      {body}
+      <tr>
+        {columns}
+      </tr>
     </tbody>
   </table>
 }
